bc458b60b7c54e121920b545c4e584091f97ea4a
sama shehata <samaashehata73@gmail.com>
2021-04-17 15:48:36 +0200

    first commit of the web project

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+function adoptionForm() {
+    document.getElementById("adoptionForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementsByClassName("popupForm").style.display = "none";
+}
+
+function dogsForm() {
+    document.getElementById("dogForm").style.display = "block";
+  }
+
+  function catsForm() {
+    document.getElementById("catForm").style.display = "block";
+  }
+
+  function hamstersForm() {
+    document.getElementById("hamsterForm").style.display = "block";
+}
+
+function birdsForm() {
+    document.getElementById("birdsForm").style.display = "block";
+}
+
+function fishsForm() {
+    document.getElementById("fishsForm").style.display = "block";
+}
+
+function rabbitsForm() {
+    document.getElementById("rabbitsForm").style.display = "block";
+}
+  
